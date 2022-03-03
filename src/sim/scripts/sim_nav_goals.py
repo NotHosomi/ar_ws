51917175ad4d262c4a7c4887f5441e4190b012b9
Hosomi <surendaotoko@gmail.com>
2022-03-03 20:11:27 +0000

    Begin traversal (cant test, sim not launching)

--- src/sim/scripts/sim_nav_goals.py
+++ src/sim/scripts/sim_nav_goals.py
@@ -8,7 +8,11 @@ from geometry_msgs.msg import PoseStamped
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
+
 class sim_nav_goals():
+
+    waypoints = [[1,2], [3.5,4.3]] #etc etc (last should be [0,0])
+
     def __init__(self):
         rospy.init_node('sim_nav_goals', anonymous=True)
 
@@ -17,7 +21,7 @@ class sim_nav_goals():
         client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
         client.wait_for_server()
 
-        self.begin = 0
+        self.begin = 0  # This variable is never used
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():            
         
@@ -26,14 +30,24 @@ class sim_nav_goals():
             msg.header.frame_id = 'map'
             msg.pose.position.z = 0
             msg.pose.orientation.w = 1
-            if self.counter % 2 == 0:
+            #if self.counter % 2 == 0:
                 # First pose
-                msg.pose.position.x = 0.0
-                msg.pose.position.y = 0.5
+            #    msg.pose.position.x = 0.0
+            #    msg.pose.position.y = 0.5
+            #else:
+                # Second pose
+            #    msg.pose.position.x = -3.0
+            #    msg.pose.position.y = 1.0
+            
+            # GOTO waypoint i
+            #msg.pose.position.x = self.waypoints[i][0]
+            #msg.pose.position.y = self.waypoints[i][1]
+            if self.counter  > len(waypoints):
+                msg.pose.position.x = self.waypoints[self.counter][0]
+                msg.pose.position.y = self.waypoints[self.counter][0]
             else:
-               # Second pose
-               msg.pose.position.x = -3.0
-               msg.pose.position.y = 1.0
+                # Reached end of path
+
                         
             msg.header.stamp = rospy.Time.now()
             goal = MoveBaseGoal()
@@ -41,12 +55,13 @@ class sim_nav_goals():
             goal.target_pose.header.stamp = rospy.Time.now()
             goal.target_pose = msg
             client.send_goal(goal)
-            wait = client.wait_for_result()
+            wait = client.wait_for_result() # This line blocks until the goal is reached, so interupt may be more difficult
     
             if not wait:
                 rospy.logerr("Action server is not available.")
                 rospy.signal_shutdown("Action server is not available.")
             else:
+                rospy.logdebug(client.get_result())
                 x = client.get_result()
                 
             rate.sleep()
