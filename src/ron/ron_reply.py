bf8d8de43d4782a41d49d02063e2856c89a46b56
JamesReddaway <cm21700@bristol.ac.uk>
2022-03-16 18:27:17 +0000

    update ron_reply

--- src/ron/ron_reply.py
+++ src/ron/ron_reply.py
@@ -1,29 +1,41 @@
 #!/usr/bin/env python
 
+from email import message
 import rospy
 import random
 from std_msgs.msg import String
+import time 
 
 # possible responses that can be given, or we could change this 
 # to return ints for different cases 
 responses = ['I am alright',
              'I need help']
 
+message_recieved = String()
+
+def callback(data):
+    # print what the Turtlebot said
+    rospy.loginfo(rospy.get_caller_id() + "Turtlebot said something: %s", data.data)
+    # store the recieved message
+    message_recieved.data = data.data
+
 def respond():
     pub = rospy.Publisher('ron_response', String, queue_size=10)
+    rospy.Subscriber('ask_ron', String, callback)
     rospy.init_node('ron_node', anonymous=True)
 
-    # rate that messages are published (Hz) (will be changed to trigger when 
-    # Ron is asked a question)
-    rate = rospy.Rate(1) 
-
     while not rospy.is_shutdown():
-        response = "%s" % responses[random.randint(0, len(responses)-1)]
-        rospy.loginfo(response)
-        pub.publish(response)
-        rate.sleep()
+	# read the message from 'ask_ron' subscriber and return a random response if Ron is asked "Are you alright Ron?"
+	# extra cases can be added if the Turtlebot says something different 
+        if message_recieved.data == "Are you alright Ron?":
+            response = "%s" % responses[random.randint(0, len(responses)-1)]
+            rospy.loginfo(response)
+            pub.publish(response)
+            message_recieved.data = None
+            
 
 if __name__ == '__main__':
+
     try:
         respond()
     except rospy.ROSInterruptException:
